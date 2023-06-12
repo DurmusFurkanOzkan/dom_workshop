64047a00ab0a9807e8182426d70c61cccd072d37
Furkan <furkandfo8@gmail.com>
2023-06-12 16:12:07 +0200

    selectors completed

---
+++ 1-selectors.js
@@ -0,0 +1,86 @@
+
+//?  ElementsByID   //
+
+// const myHeader = document.getElementById("header");
+
+// myHeader.style.color = "red";
+// myHeader.style.backgroundColor = "blue"
+
+// const button = document.getElementById("btn");
+// button.style.backgroundColor = "black";
+// button.style.color = "white";
+// button.style.padding = "10px";
+
+
+//?   TAGS  //
+
+// const list = document.getElementsByTagName("li");
+// const itemList = document.getElementsByClassName("list");
+
+
+
+// console.log(list);
+
+// list[0].style.color = "red";
+// list[list.length - 1].style.color = "orange";
+
+// list[3].innerHTML = "Sezer"
+
+// console.log(list[2].textContent);
+// console.log(list[2].innerHTML);
+
+// list[4].innerHTML += "<li>sezeeerrr</li>" ;
+
+
+//? Convert Array with Array From//
+
+
+// const itemList = document.getElementsByClassName("list");
+
+// const itemListArr = Array.from(itemList);
+
+// itemListArr.forEach((item) => console.log(item));
+
+
+//? Convert Array with spread Operator //
+
+// const itemList = document.getElementsByClassName("list")
+
+// const itemListSpread = [...itemList];
+
+// itemListSpread.map( (item) => console.log(item.style.color = "red"));
+
+//? querySelector //
+
+// document.querySelector("h1");
+// document.querySelector(".add-item");
+// document.querySelector("#header");
+
+//? querySelectorAll //
+
+// const lists = document.querySelectorAll("ul li");
+
+// lists.forEach((li) => console.log(li.innerText));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
